e9830669cf1b9fe4be2040bb6cc58d07ccded57a
seth12619 <seth_12619@yahoo.com.ph>
2016-09-22 01:08:31 +0800

    WASD - Jino's
    - PlayerController by Jino

---
+++ Survive Sink/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 1000.0f;
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void FixedUpdate()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb = GetComponent<Rigidbody>();
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+
+        rb.AddForce(move * speed);
+
+
+	}
+}
