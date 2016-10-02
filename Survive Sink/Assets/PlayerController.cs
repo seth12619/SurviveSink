9455954631d2d9769728cb23d6090c7aeb0e4c13
Jino <breginald.basilio@yahoo.com>
2016-10-02 22:46:30 +0800

    Controls WASD

--- Survive Sink/Assets/PlayerController.cs
+++ Survive Sink/Assets/PlayerController.cs
@@ -3,7 +3,17 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed = 1000.0f;
+	public float speed = 6.0f;
+	public float maxChange = 10.0f;
+	public float gravity = 10.0f;
+	public float frontInput;
+	public float sideInput;
+	public bool jumpInput;
+	public bool canJmp;
+	public float jumpHeight = 0.5f;
+	public bool grounded = false;
+
+	public GameObject player;
     public Rigidbody rb;
 
 	// Use this for initialization
@@ -11,23 +21,48 @@ public class PlayerController : MonoBehaviour {
 	
 	}
 
-    void FixedUpdate()
-    {
-
-    }
+	void Awake()
+	{
+		player = GameObject.FindWithTag ("Player");
+		rb = player.GetComponent (typeof(Rigidbody)) as Rigidbody;
+		rb.useGravity = false;
+		rb.freezeRotation = true;
+	}
 	
 	// Update is called once per frame
 	void Update () {
-        rb = GetComponent<Rigidbody>();
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+		sideInput = Input.GetAxis("Horizontal");
+		frontInput = Input.GetAxis("Vertical");
+		jumpInput = Input.GetButton ("Jump");
+	}
 
-        Vector3 move = new Vector3(moveHorizontal, 0.0f, moveVertical);
+	void FixedUpdate(){
+		if (grounded) {
 
+			Vector3 target = new Vector3 (sideInput, 0, frontInput);
+			target = transform.TransformDirection (target);
+			target *= speed;
 
-        rb.AddForce(move * speed);
+			Vector3 velocity = rb.velocity;
+			Vector3 delta = target - velocity;
+			delta.x = Mathf.Clamp (delta.x, -maxChange, maxChange);
+			delta.z = Mathf.Clamp (delta.z, -maxChange, maxChange);
+			delta.y = 0;
+			rb.AddForce (delta, ForceMode.VelocityChange);
 
+			if (canJmp && jumpInput) {
+				rb.AddForce (velocity.x, (2 * jumpHeight * gravity), velocity.z);
+			}
 
+			rb.AddForce (new Vector3 (0, -gravity * rb.mass, 0));
+
+			grounded = false;
+		}
+	}
+
+	void OnCollisionStay()
+	{
+		grounded = true;
 	}
 }
