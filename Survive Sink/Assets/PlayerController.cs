44540fe14ebc2e441b683c3f64beb54ebdafa6da
Jino <breginald.basilio@yahoo.com>
2016-10-02 22:10:54 +0800

    Revert "Made the new controls with the new library"
    This reverts commit edfcbddc9eed9f906a8e7184c373af794fab8233.

--- Survive Sink/Assets/PlayerController.cs
+++ Survive Sink/Assets/PlayerController.cs
@@ -3,16 +3,7 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 6.0f;
-	public float gravity = 10.0f;
-	public float maxChange = 10.0f;
-	public bool canJmp = true;
-	public float jumpHeight = 1.25f;
-	public bool grounded = false;
-	public float frontInput;
-	public float sideInput;
-	public bool jumpInput;
-	public GameObject player;
+    public float speed = 1000.0f;
     public Rigidbody rb;
 
 	// Use this for initialization
@@ -20,51 +11,23 @@ public class PlayerController : MonoBehaviour {
 	
 	}
 
-	void Awake()
-	{
-		player = GameObject.FindWithTag ("Player");
-		rb = player.GetComponent (typeof(Rigidbody)) as Rigidbody;
-		rb.freezeRotation = true;
-		rb.useGravity = false;
-	}
-		
+    void FixedUpdate()
+    {
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        rb = GetComponent<Rigidbody>();
 
-		sideInput = Input.GetAxis ("Horizontal");
-		frontInput = Input.GetAxis ("Vertical");
-		jumpInput = Input.GetButton ("Jump");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+
+        rb.AddForce(move * speed);
+
+
 	}
-
-	void FixedUpdate(){
-		if (grounded) {
-			Vector3 target = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
-			target = transform.TransformDirection (target);
-			target *= speed;
-
-			Vector3 velocity = rb.velocity;
-			Vector3 velocityDelta = (target - velocity);
-			velocityDelta.x = Mathf.Clamp (velocityDelta.x, -maxChange, maxChange);
-			velocityDelta.z = Mathf.Clamp (velocityDelta.z, -maxChange, maxChange);
-			velocityDelta.y = 0;
-			rb.AddForce (velocityDelta, ForceMode.VelocityChange);
-
-			//Jump
-			if (canJmp && jumpInput) {
-				rb.velocity = new Vector3(velocity.x, Mathf.Sqrt(2 * jumpHeight * gravity), velocity.z) ;
-			}
-
-			//Gravity application
-			rb.AddForce(new Vector3(0, - gravity * rb.mass, 0));
-
-			grounded = false;
-		}
-			
-	}
-	void OnCollisionStay(){
-		grounded = true;
-	}
-
-		
 }
