f0c018c98c7de308187395f29bcdfd65f47b0f91
xvhcm <harondinvincible@gmail.com>
2016-10-07 04:16:24 +0800

    Fixed Item Dropping and Display and Adjusted Door Code

--- Survive Sink/Assets/Scripts/Items/ItemDetection.cs
+++ Survive Sink/Assets/Scripts/Items/ItemDetection.cs
@@ -5,14 +5,14 @@ public class ItemDetection : Detection {
 
     public override void doAction(RaycastHit hit)
     {
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             GameObject item = hit.transform.gameObject;
 
             ItemPickup itemPickedUp = item.GetComponent<ItemPickup>();
 
             StartCoroutine(itemPickedUp.attachToPlayerLeft());
-        } else if (Input.GetKey(KeyCode.R))
+        } else if (Input.GetKeyDown(KeyCode.R))
         {
             GameObject item = hit.transform.gameObject;
 
